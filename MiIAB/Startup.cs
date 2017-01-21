bef8cd0c27560d0ecfbd9ab2704c14e55a6620b4
zntk <zntklol@gmail.com>
2017-01-21 11:12:11 +0100

    Init

---
+++ MiIAB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiIAB.Startup))]
+namespace MiIAB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
